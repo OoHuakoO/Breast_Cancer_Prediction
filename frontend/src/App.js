798aa56fbe0fdee9759d395951b27ff9fdd6cc6e
huak <komsark_@hotmail.co.th>
2021-05-05 14:39:02 +0700

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// import package and file
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./home";
+import History from "./history";
 
-function App() {
+import "./app.css";
+// ที่รวม Routh ต่างๆ
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/history" exact>
+          <History />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
-
+};
 export default App;
---
+++ frontend/src/history.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const History = () => {
+  return <div></div>;
+};
+
+export default History;
