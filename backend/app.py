8fa5dedf08e5bfc64d1ffffe8c9998905f2af70e
huak <komsark_@hotmail.co.th>
2021-05-23 01:50:55 +0700

    commit

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
 import pickle
-from sklearn.linear_model import LogisticRegression
 
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 CORS(app)
-
 
 @app.route('/prediction', methods=['POST'])
 def prediction():
@@ -24,22 +21,22 @@ def prediction():
         normalNucleoli = data['normalNucleoli']
         mitoses = data['mitoses']
         machineLearning = data['machineLearning']
-        print(machineLearning)
+
         predictData = [[float(thickness), float(cellSize),
-                         float(cellShape), float(adhesion), float(epithelialSize), float(bareNiclei), float(blandCromatin), float(normalNucleoli), float(mitoses)]]
-        if machineLearning == 'Naivebayes' :    
-                print('a')
-                naiveBayes = pickle.load(open('../model/Naivebayes.pkl', 'rb'))
-                predictionModel = naiveBayes.predict(predictData)[0]
-        elif machineLearning == 'Quadratic Discriminant Analysis' :
-                print('b')
-                quadraticDiscriminantAnalysis = pickle.load(open('../model/QuadraticDiscriminantAnalysis.pkl', 'rb'))
-                predictionModel = quadraticDiscriminantAnalysis.predict(predictData)[0]
-        elif machineLearning == 'Random Forest' :
-                print('c')
-                randomForest = pickle.load(open('../model/RandomForest.pkl', 'rb'))
-                predictionModel = randomForest.predict(predictData)[0]
+                        float(cellShape), float(adhesion), float(epithelialSize), float(bareNiclei), float(blandCromatin), float(normalNucleoli), float(mitoses)]]
+        if machineLearning == 'Naivebayes':
+            naiveBayes = pickle.load(open('../model/Naivebayes.pkl', 'rb'))
+            predictionModel = naiveBayes.predict(predictData)[0]
+        elif machineLearning == 'Quadratic Discriminant Analysis':
+            quadraticDiscriminantAnalysis = pickle.load(
+                open('../model/QuadraticDiscriminantAnalysis.pkl', 'rb'))
+            predictionModel = quadraticDiscriminantAnalysis.predict(predictData)[
+                0]
+        elif machineLearning == 'Random Forest':
+            randomForest = pickle.load(open('../model/RandomForest.pkl', 'rb'))
+            predictionModel = randomForest.predict(predictData)[0]
         print(predictionModel)
     return str(predictionModel)
+
 if __name__ == '__main__':
     app.run()
