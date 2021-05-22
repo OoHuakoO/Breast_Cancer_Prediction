33af757a0d441b871cb6bf69cbf326829f4325c4
sokmok000 <sokmok000@gmail.com>
2021-05-22 18:37:54 +0700

    ccs

---
+++ backend/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request
+import pickle
+from sklearn.linear_model import LogisticRegression
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+print("ok")
+
+@app.route('/prediction', methods=['POST'])
+def prediction():
+    if request.method == 'POST':
+        print("2")
+        data = request.get_json()
+        name = data['name']
+        surname = data['surname']
+        sex = data['sex']
+        age = data['age']
+        thickness = data['thickness']
+        cellSize = data['cellSize']
+        cellShape = data['cellShape']
+        adhesion = data['adhesion']
+        epithelialSize = data['epithelialSize']
+        bareNiclei = data['bareNiclei']
+        blandCromatin = data['blandCromatin']
+        normalNucleoli = data['normalNucleoli']
+        mitoses = data['mitoses']
+        machineLearning = data['machineLearning']
+
+        predictData = [[float(name), float(surname), float(sex), float(age), float(thickness), float(cellSize),
+                         float(cellShape), float(adhesion), float(epithelialSize), float(bareNiclei), float(blandCromatin), float(normalNucleoli), float(mitoses), float(machineLearning)]]
+        if machineLearning == 'naivebayes' :
+                print('a')
+                naiveBayes = pickle.load(open('Naivebayes.pkl', 'rb'))
+                prediction2 = naiveBayes.prediction(predictData)[0]
+                return str(prediction2)
+        elif machineLearning == 'Quadratic Discriminant Analysis' :
+                print('b')
+                quadraticDiscriminantAnalysis = pickle.load(open('QuadraticDiscriminantAnalysis.pkl', 'rb'))
+                prediction2 = quadraticDiscriminantAnalysis.prediction(predictData)[0]
+                return str(prediction2)
+        elif machineLearning == 'Random Forest' :
+                print('c')
+                randomForest = pickle.load(open('RandomForest.pkl', 'rb'))
+                prediction2 = randomForest.prediction(predictData)[0]
+                return str(prediction2)
+if __name__ == '__main__':
+    app.run()
--- frontend/src/home.js
+++ frontend/src/home.js
@@ -2,12 +2,50 @@ import React, { useState } from "react";
 import "./home.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Modal,Button } from 'react-bootstrap';
+import axios from "axios"
 
 const Home = () => {
   const [show, setShow] = useState(false);
 
+    const [name, setName] = useState();
+    const [surname, setSurname] = useState();
+    const [sex, setSex] = useState();
+    const [age, setAge] = useState();
+    const [thickness, setThickness] = useState();
+    const [cellSize, setCellSize] = useState();
+    const [cellShape, setCellShape] = useState();
+    const [adhesion, setAdhesion] = useState();
+    const [epithelialSize, setEpithelialSize] = useState();
+    const [bareNiclei, setBareNiclei] = useState();
+    const [blandCromatin, setBlandCromatin] = useState();
+    const [normalNucleoli, setNormalNucleoli] = useState();
+    const [mitoses, setMitoses] = useState();
+    const [machineLearning, setMachineLearning] = useState();
+
+  let data = {
+    name,
+    surname,
+    sex,
+    age,
+    thickness,
+    cellSize,
+    cellShape,
+    adhesion,
+    epithelialSize,
+    bareNiclei,
+    blandCromatin,
+    normalNucleoli,
+    mitoses,
+    machineLearning
+  }
+
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleShow = async() => {
+
+    let sentAPI = await axios.post("/prediction" , data)
+
+    setShow(true)
+  }
   
   return <div>
 
@@ -19,24 +57,39 @@ const Home = () => {
       <div className="row">
         <div className="column">
           <label for="fname" className="text1-home">Name</label><br/>
-          <input className="input-home" type="text" name="name" required/>
+          <input className="input-home" type="text" name="name" required 
+          onChange={(e) => {
+            setName(e.target.value)
+          }}/>
         </div>
         <div className="column">
           <label className="text1-home">Surname</label><br/>
-          <input className="input-home" type="text" name="lname" required/>
+          <input className="input-home" type="text" name="lname" required 
+          onChange={(e) => {
+            setSurname(e.target.value)
+          }}/>
         </div>
       </div>
       <div className="row">
         <div className="column">
           <label className="text1-home">Sex</label><br/>
-          <input type="radio" id="male" name="gender" value="male" className="radio-home"/>
+          <input type="radio" id="male" name="gender" value="male" className="radio-home" 
+          onChange={(e) => {
+            setSex(e.target.value)
+          }}/>
           <label for="male" className="male">Male</label>
-          <input type="radio" id="female" name="gender" value="female" className="radio-home"/>
+          <input type="radio" id="female" name="gender" value="female" className="radio-home" 
+          onChange={(e) => {
+            setSex(e.target.value)
+          }}/>
           <label for="female">Female</label>
         </div>
         <div className="column">
           <label className="text1-home">Age</label><br/>
-          <input className="input-home" type="text" name="age" required/>
+          <input className="input-home" type="text" name="age" required 
+          onChange={(e) => {
+            setAge(e.target.value)
+          }}/>
         </div>
       </div>
     </div>
@@ -47,55 +100,85 @@ const Home = () => {
       <div className="row">
         <div className="column">
           <label for="thickness" className="text1-home">Thickness</label>
-          <input className="input-home" type="text" name="thickness" required/>
+          <input className="input-home" type="text" name="thickness" required 
+          onChange={(e) => {
+            setThickness(e.target.value)
+          }}/>
         </div>
         <div className="column">
           <label for="cellsize" className="text1-home">Cell size</label>
-          <input className="input-home" type="text" name="cellsize" required/>
+          <input className="input-home" type="text" name="cellsize" required 
+          onChange={(e) => {
+            setCellSize(e.target.value)
+          }}/>
         </div>
       </div>
 
       <div className="row">
         <div className="column">
           <label for="cellshape" className="text1-home">Cell shape</label>
-          <input className="input-home" type="text" name="cellshape" required/>
+          <input className="input-home" type="text" name="cellshape" required 
+          onChange={(e) => {
+            setCellShape(e.target.value)
+          }}/>
         </div>
         <div className="column">
           <label for="adhesion" className="text1-home">Adhesion</label>
-          <input className="input-home" type="text" name="adhesion" required/>
+          <input className="input-home" type="text" name="adhesion" required 
+          onChange={(e) => {
+            setAdhesion(e.target.value)
+          }}/>
         </div>
       </div>
 
       <div className="row">
         <div className="column">
           <label for="epithelialsize" className="text1-home">Epithelial size</label>
-          <input className="input-home" type="text" name="epithelialsize" required/>
+          <input className="input-home" type="text" name="epithelialsize" required 
+          onChange={(e) => {
+            setEpithelialSize(e.target.value)
+          }}/>
         </div>
         <div className="column">
           <label for="bareniclei" className="text1-home">Bare niclei</label>
-          <input className="input-home" type="text" name="bareniclei" required/>
+          <input className="input-home" type="text" name="bareniclei" required 
+          onChange={(e) => {
+            setBareNiclei(e.target.value)
+          }}/>
         </div>
       </div>
 
       <div className="row">
         <div className="column">
           <label for="blandcromatin" className="text1-home">Bland cromatin</label>
-          <input className="input-home" type="text" name="blandcromatin" required/>
+          <input className="input-home" type="text" name="blandcromatin" required 
+          onChange={(e) => {
+            setBlandCromatin(e.target.value)
+          }}/>
         </div>
         <div className="column">
           <label for="normalnucleoli" className="text1-home">Normal nucleoli</label>
-          <input className="input-home" type="text" name="normalnucleoli" required/>
+          <input className="input-home" type="text" name="normalnucleoli" required
+          onChange={(e) => {
+            setNormalNucleoli(e.target.value)
+          }}/>
         </div>
       </div>
 
       <div className="row">
         <div className="column">
           <label for="mitoses" className="text1-home">Mitoses</label>
-          <input className="input-home" type="text" name="mitoses" required/>
+          <input className="input-home" type="text" name="mitoses" required 
+          onChange={(e) => {
+            setMitoses(e.target.value)
+          }}/>
         </div>
         <div className="column">
           <label for="classlabel" className="text1-home">Machine learning</label>
-          <select name="ml" className="input-home" required>
+          <select name="ml" className="input-home" required 
+          onChange={(e) => {
+            setMachineLearning(e.target.value)
+          }}>
             <option value="0">Naivebayes</option>
             <option value="1">Quadratic Discriminant Analysis</option>
             <option value="2">Random Forest</option>
